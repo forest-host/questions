ba47790aa636f6fe1971e4b9742607e064e99a10
Johan Schuijt <johan@publicpixels.nl>
2020-03-22 17:27:40 +0100

    add some basic package logic so we can require this list in other modules

---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+import require_dir from 'require-dir';
+
+export const defaults = require_dir('../defaults', { recurse: true });
+export const questionaires = require_dir('../questionaires', { recurse: true });
+
